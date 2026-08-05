c0a10dac0ff70f00c34957ab413bc5e9fbb8c568
nise14 <nise14@hotmail.com>
2026-08-05 15:55:35 +0000

    add Skills project with basic addition functionality to Calculate numbers

---
+++ code/Skills/Program.cs
@@ -0,0 +1,11 @@
+﻿void CalculateNumbers(int var1, int var2)
+{
+    int result = var1 + var2;
+    Console.WriteLine($"The result of adding {var1} and {var2} is: {result}");
+}
+
+Console.WriteLine("Enter the first number:");
+var num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+var num2 = Convert.ToInt32(Console.ReadLine());
+CalculateNumbers(num1, num2);
